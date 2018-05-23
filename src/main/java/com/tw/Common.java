b5e94d4e9266b65d89bf3e7121924bc6f90d802a
shuhan <2472520650@qq.com>
2018-05-23 09:11:01 +0800

    common

---
+++ src/main/java/com/tw/Common.java
@@ -0,0 +1,18 @@
+package com.tw;
+
+public class Common {
+    public static final String MAIN_INPUT =
+                    "1. 添加学生\n" +
+                    "2. 生成成绩单\n" +
+                    "3. 退出请输入你的选择（1～3）：";
+    public static String INPUT1_1 ="请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：";
+    public static String INPUT1_FALSE ="请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：";
+    public static String INPUT1_CORRECT ="学生xxx的成绩被添加";
+    public static String INPUT2_1 ="请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：";
+    public static final String PrintScore= "成绩单\n" +
+            "姓名|数学|语文|英语|编程|平均分|总分\n" +
+            "========================";
+    public static final String PrintLine = "========================";
+    public static final String TotalScore = "全班总分平均数：";
+    public static final String MedianScore = "全班总分中位数：";
+}
